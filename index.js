8ac517378e6234c6d92d352615d7493c76ed53c2
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-19 21:23:14 +0530

    routes structure updated

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ import setupDb from './startup/db.js';
 import requestConditions from './startup/requestConditions.js';
 import logger from './startup/logging.js';
 import setupCloudinary from './startup/cloudinary.js';
-import routerInit from "./routes/index.js";
-
+import intializeRoutes from './startup/routes.js';
 
 setupDb();
 requestConditions(app);
 logger(app);
 setupCloudinary();
-routerInit(app);
+intializeRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
