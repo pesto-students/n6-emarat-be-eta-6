4cb4714ceafe71ea5f3cdb213c8d9858a8cf36ef
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-19 21:48:23 +0530

    config changed to lib

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ import express from 'express';
 const app = express();
 import setupDb from './startup/db.js';
 import requestConditions from './startup/requestConditions.js';
-import logger from './startup/logging.js';
 import setupCloudinary from './startup/cloudinary.js';
 import routerInit from "./routes/index.js";
 
 
 setupDb();
 requestConditions(app);
-logger(app);
 setupCloudinary();
 routerInit(app);
 
