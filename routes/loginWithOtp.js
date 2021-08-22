5b76bc3f9cc17d6fdecaa0bee8afde6595086de1
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-22 15:44:08 +0530

    UPDATE: otp api bug fixes

--- routes/loginWithOtp.js
+++ routes/loginWithOtp.js
@@ -1,18 +1,24 @@
 import { differenceInMinutes } from "date-fns";
 import express from "express";
-import { OTP_EXPIRE_TIME_IN_MINUTES, TEST_MASTER_OTP } from "../lib/constants.js";
+import { CUSTOM_API_CODES, OTP_EXPIRE_TIME_IN_MINUTES, TEST_MASTER_OTP } from "../lib/constants.js";
 import { createToken, generateOtp, getResponseErrorFormat, getResponseFormat } from "../lib/utils.js";
 import { loginJoiSchema, loginModel } from "../models/loginWithOtp.js";
 const router = express.Router();
 import { User } from '../models/user.js';
 
 router.post('/otp', async (req, res) => {
-    const { error } = loginJoiSchema.validate(req.body);
+    const { body = {} } = req;
+    const { phone } = body;
+    const { error } = loginJoiSchema.validate(body);
     if (error) 
         return res.status(400).send(getResponseErrorFormat(error.details[0].message, '400'));
 
+    const user = await User.findOne({ phone });
+    if (!user) 
+        return res.status(400).send(getResponseErrorFormat('Invalid phone number', '400'));
+
     const otp = generateOtp();
-    const mobileWithOtp =  new loginModel({phone: req.body.phone, otp })
+    const mobileWithOtp =  new loginModel({phone, otp })
     const savedOtpObj = await mobileWithOtp.save();
 
     const { _id, createdAt } = savedOtpObj;
@@ -55,7 +61,7 @@ router.post('/loginwithotp', async (req, res) => {
 
     const { isAdmin = false } = user;
     const token = await createToken({uid: phone, additionalClaims: {isAdmin}});
-    res.send(getResponseFormat({token}));
+    res.send(getResponseFormat({token}, CUSTOM_API_CODES.AUTH_TOKEN));
 });
 
 
