8308e08cea32ef2ff1ed5d82a1f7a60d03b0ece3
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-28 00:37:28 +0530

    broadcasts api added

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 import auth from "../routes/auth.js";
 import users from "../routes/user.js";
 import amenity from "../routes/amenity.js";
+import broadcasts from "../routes/broadcasts.js";
 import uploadImage from "../routes/uploadImage.js";
 import httpError from "../middleware/httpError.js";
 
@@ -8,10 +9,9 @@ export default (app) => {
 	app.use("/", auth);
 	app.post("/upload", uploadImage);
 	app.use("/users", users);
-
 	app.use("/api/amenities", amenity);
+    app.use('/broadcasts', broadcasts)
 
 	app.use((req, res, next) => res.status(404).send("Not found"));
-
 	app.use(httpError);
 };
--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -4,7 +4,6 @@ import Formidable from 'formidable';
 export default (req, res) => {
     const form = new Formidable();
     form.parse(req, (err, fields, files) => {
-        console.log({ files })
 
         // Find Cloudinary documentation using the link below
         // https://cloudinary.com/documentation/upload_images
@@ -12,7 +11,6 @@ export default (req, res) => {
 
             // This will return the output after the code is exercuted both in the terminal and web browser
             // When successful, the output will consist of the metadata of the uploaded file one after the other. These include the name, type, size and many more.
-            console.log(result)
             if (result.public_id) {
 
                 // The results in the web browser will be returned inform of plain text formart. We shall use the util that we required at the top of this code to do this.
