a884f5312d4ec9f9985cbcf81359ff3d433abab1
Haris Rahman <iharisrahman@yahoo.com>
2021-08-29 21:48:24 +0530

    remove unkowns when validating through joi

--- helpers/joi.js
+++ helpers/joi.js
@@ -1,5 +1,5 @@
 export const joiValidator = (schema, body, res) => {
-	const { error, value } = schema.validate(body);
+	const { error, value } = schema.validate(body, { stripUnknown: true });
 
 	if (error) res.status(400).json(error.details[0].message);
 
--- requests/amenity.js
+++ requests/amenity.js
@@ -6,7 +6,6 @@ const schema = Joi.object({
 	description: Joi.string().min(5).max(500).required(),
 	fee: Joi.number().integer().min(0).max(9999).required(),
 	icon: Joi.string().min(5).max(200).required(),
-	createdAt: Joi.date(),
 });
 
 export default (req, res) => joiValidator(schema, req.body, res);
