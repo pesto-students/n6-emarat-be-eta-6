bda9d435f1fa9e3c00144384da540766753e6ad6
Haris Rahman <iharisrahman@yahoo.com>
2021-09-05 18:58:17 +0530

    bug fixes, auth token name changed for request

--- helpers/joi.js
+++ helpers/joi.js
@@ -3,7 +3,10 @@ import Joi from "joi";
 export const joiValidator = (schema, body, res) => {
 	const { error, value } = schema.validate(body, { stripUnknown: true });
 
-	if (error) res.status(400).json(error.details[0].message);
+	if (error) {
+		res.status(400).json(error.details[0].message);
+		return false;
+	}
 
 	return value;
 };
--- middleware/auth/user.js
+++ middleware/auth/user.js
@@ -2,18 +2,23 @@ import { verifyToken } from "../../config/firebaseAuth.js";
 import { getResponseErrorFormat } from "../../lib/utils.js";
 
 export default async function (req, res, next) {
-    let token = req.headers.authorization;
-    if (!token) return res.status(401).send(getResponseErrorFormat('Access denied. No token provided', 401));
-    token = token.split(' ')[1];
+	let token = req.headers.authorization;
+	if (!token)
+		return res
+			.status(401)
+			.send(
+				getResponseErrorFormat("Access denied. No token provided", 401)
+			);
+	token = token.split(" ")[1];
 
-    try {
-        const decodedToken = await verifyToken(token);
-        req.user = {
-            id: decodedToken.uniqueId,
-            isAdmin: decodedToken.isAdmin,
-        };
-        next();
-    } catch (ex) {
-        res.status(400).send(getResponseErrorFormat('Invalid Token', 400));
-    }
-};
+	try {
+		const decodedToken = await verifyToken(token);
+		req.authUser = {
+			id: decodedToken.uniqueId,
+			isAdmin: decodedToken.isAdmin,
+		};
+		next();
+	} catch (ex) {
+		res.status(400).send(getResponseErrorFormat("Invalid Token", 400));
+	}
+}
