d1f9ab9d3a553f916f0ee7796e90fbc648041fbb
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-09-05 17:16:06 +0530

    transactions feature added

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,7 @@ import broadcasts from "../routes/broadcasts.js";
 import dashboard from "../routes/dashboard.js";
 import uploadImage from "../routes/uploadImage.js";
 import payments from "../routes/payments.js";
+import transactions from "../routes/transactions.js";
 import httpError from "../middleware/httpError.js";
 import userAuth from "../middleware/auth/user.js";
 import admin from "../middleware/auth/admin.js";
@@ -19,6 +20,7 @@ export default (app) => {
 	app.use("/api/payments", payments);
 	app.use("/api/dashboard", [userAuth, admin], dashboard);
 	app.use("/api/complaints", userAuth, complaint);
+    app.use("/api/transactions", userAuth, transactions);
 
 	app.use((req, res, next) => res.status(404).send("Not found"));
 	app.use(httpError);
